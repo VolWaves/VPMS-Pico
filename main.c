592f1d3192e18a9bb69578776df8a9ddeb31af5d
xianii <xianyi.xia@outlook.com>
2022-12-07 18:37:58 +0800

    use default clock of 125Mhz

--- main.c
+++ main.c
@@ -121,17 +121,6 @@ void main_handler(uevt_t* evt) {
 #include "hardware/structs/pll.h"
 #include "hardware/structs/clocks.h"
 void clock_init(void) {
-	clock_configure(clk_sys,
-					CLOCKS_CLK_SYS_CTRL_SRC_VALUE_CLKSRC_CLK_SYS_AUX,
-					CLOCKS_CLK_SYS_CTRL_AUXSRC_VALUE_CLKSRC_PLL_USB,
-					48 * MHZ,
-					48 * MHZ);
-	pll_deinit(pll_sys);
-	clock_configure(clk_peri,
-					0,
-					CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLK_SYS,
-					48 * MHZ,
-					48 * MHZ);
 	stdio_init_all();
 }
 
